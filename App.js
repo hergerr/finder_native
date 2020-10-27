d00cbf9c143c2cc87293ff147c2c46d6af092d4a
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-10-27 13:17:00 +0100

    setup for navigation

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
+  Button,
   View,
   Text,
   StyleSheet,
-  ImageBackground
+  ImageBackground,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { COLORS } from './colors';
+
 
 const styles = StyleSheet.create({
   container: {
@@ -42,14 +39,18 @@ const styles = StyleSheet.create({
 
 const image = "./assets/images/background.jpg"
 
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require(image)} style={styles.image}>
-      <Text style={styles.title}>finder</Text>
-      </ImageBackground>
-    </View>
-
+    <NavigationContainer>
+      <View style={styles.container}>
+        <ImageBackground source={require(image)} style={styles.image}>
+          <Text style={styles.title}>finder</Text>
+          <Button onPress={(e) => {
+          }} title="Klik"></Button>
+        </ImageBackground>
+      </View>
+    </NavigationContainer>
   );
 };
 
